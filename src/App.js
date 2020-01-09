888b903cdd2fa3706044219713c7e79835ffa3b9
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-09 16:45:30 -0500

    turned App into a class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardData: []
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <SearchField></SearchField>
+        <div>
+          {this.state.cardData.map((element, index) => {
+            <GiphyCard />
+          })}
+        </div>
+      </div>
+      <Pagination />
+    );
+  }
 }
 
 export default App;
